789db062e7bb536dcfdf73fe3d7e45ce52951152
neama515 <neamaallah20249@feng.bu.edu.eg>
2025-04-29 01:28:54 +0300

    BoardPage

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,23 @@ import Home from "./Components/Home/Home";
 import ProfileEdit from "./Pages/ProfileEdit/ProfileEdit";
 import ProfilePage from "./Pages/ProfilePage";
 import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
-import NoFound from './Components/NoFound/NoFound';
+import NoFound from "./Components/NoFound/NoFound";
 import Usercontext from "./Components/UserContext/UserContext";
 import { ToastContainer } from "react-toastify";
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
 import Projects from "./Pages/Projects/Projects";
-
+import Board from "./Pages/Board/Board";
+import BoardNav from "./Components/BoardNav/BoardNav";
 
 function App() {
- 
-
   const router = createBrowserRouter([
-   
-    { path: "*", element:<NoFound />},
-    { path: "/", element: <Layout />, children: [
-        { index: true, element: <Home /> },
-    ]},
+    { path: "*", element: <NoFound /> },
+    {
+      path: "/",
+      element: <Layout />,
+      children: [{ index: true, element: <Home /> }],
+    },
     { path: "/signin", element: <SignIn /> },
     { path: "/signup", element: <SignUp /> },
     {
@@ -33,8 +33,15 @@ function App() {
           <ProfilePage />
         </ProtectedRoute>
       ),
-    },    
-    { path: "/ProfileEdit", element: <ProtectedRoute><ProfileEdit /></ProtectedRoute> },
+    },
+    {
+      path: "/ProfileEdit",
+      element: (
+        <ProtectedRoute>
+          <ProfileEdit />
+        </ProtectedRoute>
+      ),
+    },
 
     {
       path: "/projects",
@@ -44,20 +51,22 @@ function App() {
         </ProtectedRoute>
       ),
     },
-
-    
+    {
+      path: "/Board/:projectId",
+      element: (
+        <ProtectedRoute>
+          <Board />
+        </ProtectedRoute>
+      ),
+    },
+    { path: "/boardNav/:projectId", element: <BoardNav /> },
   ]);
 
-  
-
-  
   return (
-   
     <Usercontext>
-    <RouterProvider router={router} />
-    <ToastContainer />
-  </Usercontext>
-       
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </Usercontext>
   );
 }
 
